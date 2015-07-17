70cb1ceb80c4889ae88ef4720d4af77fe3e36985
siva <sivajaya75@gmail.com>
2015-07-17 16:10:34 +0530

    Assets file changed for production

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717063910) do
+ActiveRecord::Schema.define(version: 20150717101212) do
 
   create_table "accessaries", force: :cascade do |t|
     t.integer  "employee_id"
@@ -49,6 +49,11 @@ ActiveRecord::Schema.define(version: 20150717063910) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string   "emp_id"
